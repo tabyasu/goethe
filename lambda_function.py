ac41cb3dc3b143e80e06bc0c6a254918fc2ab60d
Yasuhiro Tabuchi <y-tabuchi@o-uccino.jp>
2018-06-24 21:07:48 +0900

    Fix http status code check

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import requests
 
 def lambda_handler(event, context):
     url = generate_url()
-    while check_http_status_code(url) != 200:
+    while is_ng_status_code(url):
         url = generate_url()
     return url
 
@@ -18,5 +18,8 @@ def generate_url():
     number = "{0:04d}".format(random.randint(1, max_num))
     return "http://e-village.main.jp/gazou/image_gazou/" + image_prefix + number + ".jpg"
 
-def check_http_status_code(url):
-    return requests.get(url).status_code
+def is_ng_status_code(url):
+    return get_http_status_code(url) != requests.codes.ok
+
+def get_http_status_code(url):
+    return requests.get(url, allow_redirects = False).status_code
